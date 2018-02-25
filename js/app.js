9c6ddd9d534cdf26c01afe415217f96128f3cdc7
Alex Heerding <alexheerding@Alexs-MacBook-Pro.local>
2018-02-25 00:39:28 -0500

    Added suport for IE 11-8

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+"use strict";
 //get the next object from the data and call createDiv
 // params - the id of the current select field and the index of the selected option
 function getNext(selectId, id){
@@ -6,15 +7,15 @@ function getNext(selectId, id){
     next = data[0];
   } else if(id != 'blank'){
     //get the id for the next node
-    let nextId;
-    for (let i = 0; i < data.length; i++) {
+    var nextId;
+    for (var i = 0; i < data.length; i++) {
       if (data[i].id === selectId) {
         nextId = data[i].options[id].nextNode;
         break;
       }
     }
     //get the next data item
-    for (let i = 0; i < data.length; i++) {
+    for (var i = 0; i < data.length; i++) {
       if (data[i].id === nextId) {
         next = data[i];
         break;
@@ -32,19 +33,19 @@ function getNext(selectId, id){
 //creates the select based off of the passed in data
 //params - the object to render from data.js
 function createDiv(next) {
-  let header = document.createElement('h3');
+  var header = document.createElement('h3');
   header.setAttribute('class', next.id);
   header.appendChild(document.createTextNode(next.text));
   document.getElementById('body').appendChild(header);
   if (next.options != null) {
-    let select = document.createElement('select');
+    var select = document.createElement('select');
     select.setAttribute('id', next.id);
-    let def = document.createElement('option');
+    var def = document.createElement('option');
     def.appendChild(document.createTextNode('-------------'));
     def.setAttribute('value', 'blank');
     select.appendChild(def);
-    for (let i = 0; i < next.options.length; i++) {
-      let option = document.createElement('option');
+    for (var i = 0; i < next.options.length; i++) {
+      var option = document.createElement('option');
       option.appendChild(document.createTextNode(next.options[i].text));
       option.setAttribute('value', i);
       select.appendChild(option)
@@ -58,7 +59,7 @@ function removeChildren(selectId){
   //get array of all nodes in body
   var nodes = document.getElementById('body').childNodes;
   //loop through backwards
-  for(let i = nodes.length; i > 0; i--){
+  for(var i = nodes.length; i > 0; i--){
     if(i != nodes.length){
       //if the id doesn't match and we haven't reached the current node, remove
       if(nodes[i].id != selectId){
